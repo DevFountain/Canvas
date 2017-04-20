e2398fd326e32d9ad03ee72635b77df8a737fc18
Curtis Wilcox <curtis@devfountain.com>
2017-04-19 21:10:29 -0600

    Completed all milestones

--- Canvas/ViewController.swift
+++ Canvas/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var trayView: UIView!
+
+    var trayOriginalCenter: CGPoint!
+    var trayCenterWhenOpen: CGPoint!
+    var trayCenterWhenClosed: CGPoint!
+
+    var newlyCreatedFace: UIImageView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        trayCenterWhenOpen = CGPoint(x: trayView.center.x, y: view.frame.height - trayView.frame.height + 110)
+        trayCenterWhenClosed = trayView.center
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,49 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func smileyPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+
+        if sender.state == .began {
+            let imageView = sender.view as! UIImageView
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+        } else if sender.state == .changed {
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+        }
+    }
+    
+    @IBAction func onPanGesture(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    self.trayView.center = self.trayCenterWhenClosed
+                })
+            } else {
+                UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    self.trayView.center = self.trayCenterWhenOpen
+                })
+            }
+        }
+    }
+
+    @IBAction func onTapGesture(_ sender: UITapGestureRecognizer) {
+        if trayView.center == trayCenterWhenOpen {
+            trayView.center = trayCenterWhenClosed
+        } else {
+            trayView.center = trayCenterWhenOpen
+        }
+    }
 
 }
 
